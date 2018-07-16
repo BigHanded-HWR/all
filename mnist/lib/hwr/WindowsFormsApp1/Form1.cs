20d5cade9ec4d04e61e5ae2e3df1b6040c06ad18
suk <erroring@sina.cn>
2018-07-16 13:39:34 +0800

    hwr-dll

---
+++ mnist/lib/hwr/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using hwr;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            //openFileDialog.Filter = "jpg files (*.jpg)|*.jpg|All files(*.*)|*>**";
+            openFileDialog.Filter = "jpg files (*.jpg)|*.png|All files(*.*)|*>**";
+            openFileDialog.FilterIndex = 1;
+            openFileDialog.RestoreDirectory = true;
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                var a = new InferImage();
+                label1.Text=a.RecImg(openFileDialog.FileName);
+            }
+        }
+    }
+}
