6ee21c15971655dcf841a5a6152362fd0b49a73c
1160300131 <36832878+1160300131@users.noreply.github.com>
2018-07-21 01:18:23 +0800

    bitmap

---
+++ code/bmpWrite/bmpWrite/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace bmpWrite
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.clean = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.work = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, -1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(420, 420);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // clean
+            // 
+            this.clean.Location = new System.Drawing.Point(553, 367);
+            this.clean.Name = "clean";
+            this.clean.Size = new System.Drawing.Size(136, 41);
+            this.clean.TabIndex = 1;
+            this.clean.Text = "清除";
+            this.clean.UseVisualStyleBackColor = true;
+            this.clean.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(466, 27);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(297, 230);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // work
+            // 
+            this.work.Location = new System.Drawing.Point(553, 307);
+            this.work.Name = "work";
+            this.work.Size = new System.Drawing.Size(136, 44);
+            this.work.TabIndex = 3;
+            this.work.Text = "分析";
+            this.work.UseVisualStyleBackColor = true;
+            this.work.Click += new System.EventHandler(this.Work_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.work);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.clean);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button clean;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button work;
+    }
+}
+
---
+++ code/bmpWrite/bmpWrite/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bmpWrite
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private Bitmap digitImage;//用来保存手写数字
+        private Bitmap grayTmp;
+        private Point startPoint;//用于绘制线段，作为线段的初始端点坐标
+        private const int MnistImageSize = 28;//Mnist模型所需的输入图片大小
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //当点击清除时，重新绘制一个白色方框，同时清除label1显示的文本
+            digitImage = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            Graphics g = Graphics.FromImage(digitImage);
+            g.Clear(Color.Black);
+            pictureBox1.Image = digitImage;
+            //pictureBox2.Image = grayTmp;
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            grayTmp = new Bitmap(28, 28);
+            digitImage = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            Graphics g = Graphics.FromImage(digitImage);
+            g.Clear(Color.Black);
+            pictureBox1.Image = digitImage;
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            //当鼠标左键被按下时，记录下需要绘制的线段的起始坐标
+            startPoint = (e.Button == MouseButtons.Left) ? e.Location : startPoint;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            //当鼠标在移动，且当前处于绘制状态时，根据鼠标的实时位置与记录的起始坐标绘制线段，同时更新需要绘制的线段的起始坐标
+            if (e.Button == MouseButtons.Left)
+            {
+                Graphics g = Graphics.FromImage(digitImage);
+                Pen myPen = new Pen(Color.White, 40);
+                myPen.StartCap = LineCap.Round;
+                myPen.EndCap = LineCap.Round;
+                g.DrawLine(myPen, startPoint, e.Location);
+                pictureBox1.Image = digitImage;
+                g.Dispose();
+                startPoint = e.Location;
+            }
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                Bitmap digitTmp = (Bitmap)digitImage.Clone();//复制digitImage
+                grayTmp = new Bitmap(28, 28);                                            //调整图片大小为Mnist模型可接收的大小：28×28
+                using (Graphics g = Graphics.FromImage(digitTmp))
+                {
+                    g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                    g.DrawImage(digitTmp, 0, 0, MnistImageSize, MnistImageSize);
+                }
+                //将图片转为灰阶图，并将图片的像素信息保存在list中
+               // var image = new List<float>(MnistImageSize * MnistImageSize);
+                for (var x = 0; x < MnistImageSize; x++)
+                {
+                    for (var y = 0; y < MnistImageSize; y++)
+                    {
+                        var color = digitTmp.GetPixel(y, x);
+                        var a = (int)((color.R + color.G + color.B) / (3.0));
+                        grayTmp.SetPixel(y, x, Color.FromArgb(a, a, a));
+                    }
+                }
+                
+
+            }
+
+        }
+        public Bitmap CaptureImage(Bitmap fromImage, int offsetX, int offsetY, int width, int height)
+        {
+           
+            //创建新图位图
+            Bitmap bitmap = new Bitmap(width, height);
+            //创建作图区域
+            Graphics graphic = Graphics.FromImage(bitmap);
+            //截取原图相应区域写入作图区
+            graphic.DrawImage(fromImage, 0, 0, new Rectangle(offsetX, offsetY, width, height), GraphicsUnit.Pixel);
+            //从作图区生成新图
+            Image saveImage = Image.FromHbitmap(bitmap.GetHbitmap());
+            //保存图片
+            return(Bitmap) saveImage;
+           // saveImage.Save(toImagePath, ImageFormat.Png);
+            //释放资源   
+            saveImage.Dispose();
+            graphic.Dispose();
+            bitmap.Dispose();
+        }
+
+        private void Work_Click(object sender, EventArgs e)
+        {
+            Boolean flag = false;
+            int m = 0;
+            var bitList = new List<Bitmap>(MnistImageSize * MnistImageSize);
+
+            for (var x = 0; x < MnistImageSize; x++)
+            {
+                for (var y = 0; y < MnistImageSize; y++)
+                {
+                    
+                    if (grayTmp.GetPixel(x, y) == Color.White)
+                    {
+                        flag = true;
+                    }
+                    Console.WriteLine(flag);
+                }
+                if (flag == false)
+                {
+                    Bitmap cut = new Bitmap(28, 28);
+                    /*for (var i = m; i < x; i++)
+                        for (var j = 0; j < MnistImageSize; j++)
+                            cut.SetPixel(i,j, grayTmp.GetPixel(i+m,j));*/
+
+                    //pictureBox2.Image = cut;
+                    bitList.Add(CaptureImage(grayTmp, m, 0, x - m + 1, MnistImageSize));
+                    //Console.WriteLine(x-m+1);
+                    m = x;
+                    //pictureBox2.Image = cut;
+                }
+                else
+                {
+
+                    flag = false;
+                }
+            }
+            if (bitList.Count() != 0)
+            {
+
+                 pictureBox2.Image = bitList[1];
+                //pictureBox2.Image = grayTmp;
+            }
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+    }
